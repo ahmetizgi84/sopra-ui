7577943b84a4086c4259b1df95939ecb2c617782
Ahmet IZGI <dev.ahmetizgi@gmail.com>
2022-10-25 14:14:34 +0300

    q fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import { Box, Flex, VStack } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import Tabbar from './components/Tabbar';
 
 function App() {
   return (
-    <Flex bg="gray.50" style={{ minHeight: '130vh' }} direction="column" justifyContent="space-between">
+    <Flex bg="gray.50" minWidth="max-content" direction="column" justifyContent="space-between">
       <Box>
         <Navbar />
         <Tabbar />
